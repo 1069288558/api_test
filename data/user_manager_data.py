94f6383d64fc2f239989f7aecf61c2baa9684dae
‘1069288558’ <1069288558@qq.com>
2020-12-21 15:51:07 +0800

    Initial commit

---
+++ data/user_manager_data.py
@@ -0,0 +1,12 @@
+
+
+
+class UserMangerData():
+
+
+    user_case_data = {
+        "username":"test021",
+        "password":"test021",
+        "new_username":"xxxx008",
+        "new_password":"xxxx008"
+    }
